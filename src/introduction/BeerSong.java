95785f096895963b41e8b4c184b897a426511d8b
Hikin <wuhaijing@yahoo.com>
2017-02-23 14:28:25 +0800

    My version of BeerSong

---
+++ src/introduction/BeerSong.java
@@ -0,0 +1,43 @@
+package introduction;
+
+/**
+ * Created by wuhaijing on 2017/2/23.
+ * My version of BeerSong
+ */
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String[] singularPluralStrings = {"bottle", "bottles"};
+        while (beerNum > 0) {
+            String word = switchSingularPluralPair(singularPluralStrings, beerNum);
+            printBeerSong(beerNum, word, true);
+            printBeerSong(beerNum, word, false);
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum -= 1;
+            word = switchSingularPluralPair(singularPluralStrings, beerNum);
+            if (isPositive(beerNum)) {
+                printBeerSong(beerNum, word, true);
+            } else {
+                System.out.println("No more bottles of beer on the wall");
+            }
+        }
+    }
+
+    private static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    private static void printBeerSong(int beerNum, String word, boolean isOnTheWall) {
+        if (isOnTheWall) {
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+        } else {
+            System.out.println(beerNum + " " + word + " of beer.");
+        }
+    }
+
+    private static String switchSingularPluralPair(String[] singularPluralStrings, int num) {
+        return num == 1 ? singularPluralStrings[0] : singularPluralStrings[1];
+    }
+}
+
